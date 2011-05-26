131f95cc537cb624d126863a0f40f80d4a4346ef
Trevor Turk <trevorturk@gmail.com>
2011-05-25 19:00:01 -0500

    fix tests

--- test/dummy/config/boot.rb
+++ test/dummy/config/boot.rb
@@ -4,7 +4,9 @@ gemfile = File.expand_path('../../../../Gemfile', __FILE__)
 if File.exist?(gemfile)
   ENV['BUNDLE_GEMFILE'] = gemfile
   require 'bundler'
-  Bundler.setup
+  require 'bundler/setup'
+  require 'rails/all'
+  require 'delayed_job'
 end
 
 $:.unshift File.expand_path('../../../../lib', __FILE__)
